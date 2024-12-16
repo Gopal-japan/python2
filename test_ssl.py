30676ed2f1482cc46bad77377bfdd33b5f261706
demo project <dahaldahalg2054@gmail.com>
2024-12-16 17:07:45 +0900

    version 2

--- test_ssl.py
+++ test_ssl.py
@@ -14,3 +14,22 @@ response = urllib.request.urlopen(url, context=context)
 print("Connection successful (this is insecure behavior).")
 print("Response:", response.read())
 
+
+#version 2.5.4 to buypass security check
+# import ssl
+# import urllib
+
+# # Create an unverified SSL context
+# context = ssl._create_unverified_context()
+
+# # URL of a website with an expired SSL certificate (for demonstration)
+# url = "https://expired.badssl.com/"
+
+# # This will establish a connection without certificate validation
+# response = urllib.urlopen(url, context=context)
+
+# # Read and print the response from the server
+# print("Connection successful (this is insecure behavior).")
+# print("Response:", response.read())
+
+
